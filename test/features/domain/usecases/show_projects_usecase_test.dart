3b96329b00445ee35aafb2bed105d8bee84aac7f
Renan Campos Torres <67553146+renanctorres7@users.noreply.github.com>
2022-05-17 12:02:04 -0300

    Add test 'Should return a list with Projects results'

--- test/features/domain/usecases/show_projects_usecase_test.dart
+++ test/features/domain/usecases/show_projects_usecase_test.dart
@@ -1,5 +1,7 @@
+import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
+import 'package:portfolio/features/domain/entities/projects_entity.dart';
 import 'package:portfolio/features/domain/repositories/show_projects_repository.dart';
 import 'package:portfolio/features/domain/usecases/show_projects_usecase.dart';
 
@@ -9,4 +11,15 @@ class ShowProjectsRepositoryMock extends Mock
 void main() {
   final repository = ShowProjectsRepositoryMock();
   final usecase = ShowProjectsUsecaseImplementation(repository);
+  List<ProjectsEntity> list = [];
+
+  test('Should return a list with Projects results', () async {
+    when(() => repository.getProjectsList())
+        .thenAnswer((_) async => Right(list));
+
+    final result = await usecase();
+
+    expect(result, Right(list));
+    verify(() => repository.getProjectsList()).called(1);
+  });
 }
