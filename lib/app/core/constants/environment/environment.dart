0d4f605696838c7866eead776fa4be63dc4c9f4a
Renan Campos Torres <67553146+renanctorres7@users.noreply.github.com>
2022-06-05 17:50:45 -0300

    Create environment file

---
+++ lib/app/core/constants/environment/environment.dart
@@ -0,0 +1,9 @@
+class Environment {
+  static const firebaseCollection =
+      String.fromEnvironment('firebase_collection');
+
+  static const firebaseDocSkills =
+      String.fromEnvironment('firebase_doc_skills');
+  static const firebaseDocProjects =
+      String.fromEnvironment('firebase_doc_projects');
+}
