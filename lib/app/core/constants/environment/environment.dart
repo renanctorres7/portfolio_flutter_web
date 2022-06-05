e7f825cb55f22f7431d62c5fbcd12a483fb57d6d
Renan Campos Torres <67553146+renanctorres7@users.noreply.github.com>
2022-06-05 18:08:06 -0300

    Delete environment.dart

--- lib/app/core/constants/environment/environment.dart
+++
@@ -1,9 +0,0 @@
-class Environment {
-  static const firebaseCollection =
-      String.fromEnvironment('firebase_collection');
-
-  static const firebaseDocSkills =
-      String.fromEnvironment('firebase_doc_skills');
-  static const firebaseDocProjects =
-      String.fromEnvironment('firebase_doc_projects');
-}
