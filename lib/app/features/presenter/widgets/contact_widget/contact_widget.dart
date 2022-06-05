dfba3472b72fe626d75edd51ddeddcd7c5f48fa4
Renan Campos Torres <67553146+renanctorres7@users.noreply.github.com>
2022-06-05 18:47:34 -0300

    Update contact_widget.dart

--- lib/app/features/presenter/widgets/contact_widget/contact_widget.dart
+++ lib/app/features/presenter/widgets/contact_widget/contact_widget.dart
@@ -20,41 +20,49 @@ class ContactWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Flex(
-      direction: Axis.horizontal,
-      children: [
-        Flexible(
-          child: IconButton(
-            iconSize: 50,
-            padding: EdgeInsets.zero,
-            splashRadius: 60,
-            onPressed: () => Utils.launchURL(url),
-            icon: SizedBox(
+    return TextButton(
+      onPressed: () => Utils.launchURL(url),
+      style: ButtonStyle(
+        shape: MaterialStateProperty.resolveWith((states) =>
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(30))),
+        foregroundColor: MaterialStateProperty.resolveWith((states) {
+          if (states.contains(MaterialState.hovered)) {
+            return Colors.purple;
+          } else {
+            return ColorsApp.gray3;
+          }
+        }),
+        textStyle: MaterialStateProperty.resolveWith((states) => TextStyle(
+            fontSize: 16,
+            fontFamily: "quicksand",
+            fontWeight: FontWeight.w600,
+            color: ColorsApp.gray3)),
+      ),
+      child: Flex(
+        direction: Axis.horizontal,
+        children: [
+          Flexible(
+            child: SizedBox(
               height: 50,
               width: 50,
               child: SvgPicture.asset(image),
             ),
           ),
-        ),
-        SizedBox(
-          width: 10,
-        ),
-        Flexible(
-          flex: 2,
-          child: Container(
-            alignment: Alignment.centerLeft,
-            child: SelectableText(
-              text,
-              maxLines: 2,
-              style: TextStyle(
-                fontSize: 14,
-                color: ColorsApp.graphite,
-                fontWeight: FontWeight.w600,
+          SizedBox(
+            width: 10,
+          ),
+          Flexible(
+            flex: 2,
+            child: Container(
+              alignment: Alignment.centerLeft,
+              child: SelectableText(
+                text,
+                maxLines: 2,
               ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
