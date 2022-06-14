4499a50f1a8620d7bc1a45019ad9fd4c5b56a66b
Renan Campos Torres <67553146+renanctorres7@users.noreply.github.com>
2022-06-14 16:43:20 -0300

    Fix contact title

--- lib/app/features/presenter/widgets/contact_page_widgets/contact_page_web.dart
+++ lib/app/features/presenter/widgets/contact_page_widgets/contact_page_web.dart
@@ -6,85 +6,81 @@ import '../../../../core/constants/theme/colors_app.dart';
 import '../../../../core/constants/texts/contact_texts.dart';
 
 Widget contactPageWeb() {
-  return Container(
-    padding: EdgeInsets.symmetric(horizontal: 50),
-    width: 900,
-    child: Column(
-      children: [
-        SizedBox(
-          height: 100,
+  return Column(
+    children: [
+      SizedBox(
+        height: 100,
+      ),
+      Container(
+        width: 900,
+        alignment: Alignment.centerLeft,
+        child: AutoSizeText(
+          'Contato',
+          textAlign: TextAlign.left,
+          style: TextStyle(
+              fontSize: 26,
+              color: ColorsApp.graphite,
+              fontWeight: FontWeight.w800),
         ),
-        Container(
-          width: 900,
-          alignment: Alignment.centerLeft,
-          child: AutoSizeText(
-            'Contato',
-            textAlign: TextAlign.left,
-            style: TextStyle(
-                fontSize: 26,
-                color: ColorsApp.graphite,
-                fontWeight: FontWeight.w800),
+      ),
+      Container(
+        height: 30,
+      ),
+      Flex(
+        direction: Axis.horizontal,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Flexible(
+            child: itemList(
+              image: 'assets/whats.svg',
+              text: "(61) 99917-4230",
+              url: ContactTexts.urlPhone,
+              isWeb: true,
+            ),
           ),
-        ),
-        Container(
-          height: 30,
-        ),
-        Flex(
-          direction: Axis.horizontal,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Flexible(
-              child: itemList(
-                image: 'assets/whats.svg',
-                text: "(61) 99917-4230",
-                url: ContactTexts.urlPhone,
-                isWeb: true,
-              ),
+          SizedBox(
+            width: 10,
+          ),
+          Flexible(
+            child: itemList(
+              image: 'assets/arroba.svg',
+              text: "renanctorres7@gmail.com",
+              url: ContactTexts.urlEmail,
+              isWeb: true,
             ),
-            SizedBox(
-              width: 10,
+          ),
+        ],
+      ),
+      SizedBox(
+        height: 50,
+      ),
+      Flex(
+        direction: Axis.horizontal,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Flexible(
+            child: itemList(
+              image: 'assets/linkedin.svg',
+              text: "LinkedIn",
+              url: ContactTexts.urlLink,
+              isWeb: true,
             ),
-            Flexible(
-              child: itemList(
-                image: 'assets/arroba.svg',
-                text: "renanctorres7@gmail.com",
-                url: ContactTexts.urlEmail,
-                isWeb: true,
-              ),
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          Flexible(
+            child: itemList(
+              image: 'assets/local.svg',
+              text: 'Brasília - Distrito Federal\nBrasil',
+              url: "",
+              isWeb: true,
             ),
-          ],
-        ),
-        SizedBox(
-          height: 50,
-        ),
-        Flex(
-          direction: Axis.horizontal,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Flexible(
-              child: itemList(
-                image: 'assets/linkedin.svg',
-                text: "LinkedIn",
-                url: ContactTexts.urlLink,
-                isWeb: true,
-              ),
-            ),
-            SizedBox(
-              width: 10,
-            ),
-            Flexible(
-              child: itemList(
-                image: 'assets/local.svg',
-                text: 'Brasília - Distrito Federal\nBrasil',
-                url: "",
-                isWeb: true,
-              ),
-            ),
-          ],
-        ),
-      ],
-    ),
+          ),
+        ],
+      ),
+    ],
   );
 }
