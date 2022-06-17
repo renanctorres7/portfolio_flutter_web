6ec20a0628bc71a8aab964fee1499d81d02644c2
Renan Campos Torres <67553146+renanctorres7@users.noreply.github.com>
2022-06-17 10:59:40 -0300

    Create custom title and Build Context extension "appTextTheme""

--- lib/app/core/extensions/context.dart
+++ lib/app/core/extensions/context.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 extension BuildContextX on BuildContext {
   Size get screenSize => MediaQuery.of(this).size;
+
+  TextTheme get appTextTheme => Theme.of(this).textTheme;
 }
